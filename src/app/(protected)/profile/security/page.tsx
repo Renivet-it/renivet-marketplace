3939303f18f6e0949d02697784d3f817a8cd6376
itsdrvgo <thedragoluca@gmail.com>
2024-11-06 18:38:30 +0530

    ADD: added metadata for pages

--- src/app/(protected)/profile/security/page.tsx
+++ src/app/(protected)/profile/security/page.tsx
@@ -1,4 +1,10 @@
 import { SecurityPage } from "@/components/profile";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Password & Security",
+    description: "Update your account's password and security settings",
+};
 
 export default function Page() {
     return <SecurityPage className="md:basis-3/4" />;
