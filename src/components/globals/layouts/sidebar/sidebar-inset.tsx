d7030f50ccedfe23f6548461d68435525c9a1ae8
CR7 <sourav1018.ghosh@gmail.com>
2025-04-06 19:39:58 +0530

    fix:(REN-6) breadcrumb not display product name
    fix:(REN-6) admin side breadcrumb not display product title

--- src/components/globals/layouts/sidebar/sidebar-inset.tsx
+++ src/components/globals/layouts/sidebar/sidebar-inset.tsx
@@ -17,6 +17,7 @@ import React from "react";
 export function SidebarInset({ className, ...props }: GenericProps) {
     const pathname = usePathname();
     const segments = pathname.split("/").filter(Boolean);
+    const productId = segments[segments.length - 1];
 
     const { data: brand } = trpc.general.brands.getBrand.useQuery(
         { id: segments[2] },
@@ -28,6 +29,16 @@ export function SidebarInset({ className, ...props }: GenericProps) {
         }
     );
 
+    const { data: product } = trpc.brands.products.getProduct.useQuery(
+        { productId },
+        {
+            enabled:
+                segments.length >= 6 &&
+                segments[3] === "products" &&
+                segments[4] === "p" &&
+                isValidUUID(productId),
+        }
+    );
     return (
         <Breadcrumb className={cn("", className)} {...props}>
             <BreadcrumbList>
@@ -40,7 +51,9 @@ export function SidebarInset({ className, ...props }: GenericProps) {
                         <BreadcrumbItem>
                             {index === segments.length - 1 ? (
                                 <BreadcrumbPage>
-                                    {convertValueToLabel(segment)}
+                                    {product
+                                        ? product.title
+                                        : convertValueToLabel(segment)}
                                 </BreadcrumbPage>
                             ) : (
                                 <BreadcrumbLink
