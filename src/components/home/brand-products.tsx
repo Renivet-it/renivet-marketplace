ad25cafa83e14a6e6f30ab5337cb6ba4a665d07a
CR7 <sourav1018.ghosh@gmail.com>
2025-05-08 03:51:50 +0530

    fix(ren-37): redesign the top product of section and make responsive

--- src/components/home/brand-products.tsx
+++ src/components/home/brand-products.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel";
 import { cn } from "@/lib/utils";
 import { HomeBrandProduct } from "@/lib/validations";
-import Autoplay from "embla-carousel-autoplay";
+import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -25,51 +18,114 @@ export function BrandProducts({
     return (
         <section
             className={cn(
-                "flex w-full justify-center py-5 md:px-8 md:py-10",
+                "flex w-full justify-center py-5 md:py-10",
                 className
             )}
             {...props}
         >
-            <div className="w-full max-w-5xl xl:max-w-[100rem]">
-                <Carousel
-                    plugins={[
-                        Autoplay({
-                            delay: 5000,
-                        }),
-                    ]}
-                    opts={{
-                        loop: true,
-                        align: "start",
-                    }}
-                >
-                    <CarouselContent className="-ml-2 md:-ml-4">
+            <div className="container">
+                <div className="flex justify-center gap-2">
+                    <h2
+                        className={cn(
+                            "text-xl sm:text-3xl",
+                            "text-center",
+                            "capitalize",
+                            "pb-4"
+                        )}
+                    >
+                        top-rated brand products
+                    </h2>
+                    <img
+                        src="assets/marketing/downward-rotation-svgrepo-com.svg"
+                        alt="arrow-down"
+                        className="h-10 w-10"
+                    />
+                </div>
+                <div className="scrollbar-hide flex flex-col gap-4 overflow-x-auto px-1 md:hidden">
+                    <div className="flex gap-4">
                         {brandProducts.map((item, index) => (
-                            <CarouselItem
-                                key={index}
-                                className="pl-2 md:basis-1/3 md:pl-4 lg:basis-1/6"
-                            >
-                                <Link
-                                    href={item.url || "/shop"}
-                                    className="block"
-                                >
-                                    <div className="aspect-[4/5] overflow-hidden">
-                                        <Image
-                                            src={item.imageUrl}
-                                            alt={`Brand Product ${index + 1}`}
-                                            width={200}
-                                            height={200}
-                                            className="size-full object-cover"
-                                        />
-                                    </div>
-                                </Link>
-                            </CarouselItem>
+                            <BrandCard key={index} product={item} />
                         ))}
-                    </CarouselContent>
+                    </div>
+                </div>
 
-                    <CarouselPrevious className="left-0 h-full rounded-none border-none bg-gradient-to-r from-background to-transparent hover:from-background hover:to-transparent" />
-                    <CarouselNext className="right-0 h-full rounded-none border-none bg-gradient-to-l from-background to-transparent" />
-                </Carousel>
+                <div className="hidden grid-cols-2 gap-6 md:grid lg:grid-cols-3 xl:grid-cols-4">
+                    {brandProducts.map((item, index) => (
+                        <BrandCard key={index} product={item} />
+                    ))}
+                </div>
             </div>
         </section>
+    );
+}
+
+function BrandCard({ product }: { product: HomeBrandProduct }) {
+    // const discount =
+    //     product.comparePrice && product.price
+    //         ? Math.round(
+    //               ((product.comparePrice - product.price) /
+    //                   product.comparePrice) *
+    //                   100
+    //           )
+    //         : 0;
+
+    return (
+        <Link
+            href={product.url || "/shop"}
+            className="relative w-[200px] flex-shrink-0 md:w-auto"
+        >
+            <div className="relative aspect-[3/4] w-full overflow-hidden rounded-[8px] shadow-sm">
+                <Image
+                    src={product.imageUrl}
+                    alt={"product.title"}
+                    fill
+                    className="object-cover transition-transform duration-300 hover:scale-105"
+                />
+
+                {/* Overlay */}
+                <div className="absolute inset-x-0 bottom-0 z-10 bg-[linear-gradient(rgba(0,0,0,0.004),rgb(0,0,0))] p-3 text-white rounded-[8px]">
+                    <h3 className="truncate text-[14px] font-semibold">
+                        {"product.title"}
+                    </h3>
+                    <p className="truncate text-[12px] capitalize opacity-80">
+                        {"product.category"}
+                    </p>
+
+                    <div className="mt-1 flex items-center justify-between text-sm">
+                        <div className="space-x-2">
+                            <span className="text-[12px] font-bold">
+                                ₹{"product.price"}
+                            </span>
+
+                            {product.comparePrice && (
+                                <>
+                                    <span className="text-[10px] line-through opacity-70">
+                                        ₹{product.comparePrice}
+                                    </span>
+                                    <span className="text-[10px] font-semibold text-green-400">
+                                        {/* Replace this with calculated value */}
+                                        {Math.round(
+                                            ((product.comparePrice -
+                                                product.price) /
+                                                product.comparePrice) *
+                                                100
+                                        )}
+                                        % off
+                                    </span>
+                                </>
+                            )}
+
+                            <span className="text-[10px] line-through opacity-70">
+                                ₹{"product.comparePrice"}
+                            </span>
+                            <p className="text-[10px] font-semibold text-green-400">
+                                {/* Replace this with calculated value */}
+                                {"product.discount"}% off
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Link>
     );
 }
