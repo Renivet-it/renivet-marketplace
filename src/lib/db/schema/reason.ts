5f7ddcd2fbf56b6983e38faae71e2c29b693c7bb
CR7 <sourav1018.ghosh@gmail.com>
2025-06-04 03:21:26 +0530

    feat(ren-39): add reason table for return and exchange reason

--- src/lib/db/schema/reason.ts
+++ src/lib/db/schema/reason.ts
@@ -1,3 +1,30 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { bigint, boolean, foreignKey, pgTable, text, unique, uuid } from "drizzle-orm/pg-core";
+import { typeMasters } from "./types";
+import { timestamps } from "../helper";
 
-export const causeDescription = pgTable("", {}, (t) => []);
+export const reasonMasters = pgTable(
+    "reason_master",
+    {
+        id: uuid("id").primaryKey().defaultRandom(),
+        name: text("name"),
+        description: text("description"),
+        parentId: uuid("parent_id"),
+        level: bigint("level", { mode: "number" }),
+        isActive: boolean("is_active").default(true),
+        shortOrder: bigint("short_order", { mode: "number" }),
+        reasonType: text("reason_type"),
+        ...timestamps
+    },
+    (t) => [
+        foreignKey({
+            columns: [t.parentId],
+            foreignColumns: [t.id],
+            name: "reason_master_parent_id_fkey",
+        }).onDelete("cascade"),
+        foreignKey({
+            columns: [t.reasonType],
+            foreignColumns: [typeMasters.code],
+            name: "reason_master_reason_type_fkey",
+        })
+    ]
+);
