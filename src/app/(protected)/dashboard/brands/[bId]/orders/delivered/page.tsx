3de38da1b9e8a3cf64c4207e048ac76a481a2622
ayanganguly <ayanganguly333@gmail.com>
2025-12-04 20:25:52 +0530

    ~

---
+++ src/app/(protected)/dashboard/brands/[bId]/orders/delivered/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Delivered Orders</h1>
+    </div>
+  );
+}
---
+++ src/app/(protected)/dashboard/brands/[bId]/orders/shipped/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Shipped Orders</h1>
+    </div>
+  );
+}
