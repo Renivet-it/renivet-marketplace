776b223aaf8b67717d759f20e27233bf18731239
ayanganguly <ayanganguly333@gmail.com>
2026-02-23 01:10:18 +0530

    ~

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,6 +1,7 @@
 import {
     DM_Sans,
     Josefin_Sans,
+    Lato,
     Playfair_Display,
     Rubik,
     Work_Sans,
@@ -8,35 +9,42 @@ import {
 
 export const dmsans = DM_Sans({
     subsets: ["latin"],
-    weight: ["400", "500", "600"],
+    weight: ["400", "500", "600", "800"],
     display: "swap",
     variable: "--font-dmsans",
 });
 
 export const worksans = Work_Sans({
     subsets: ["latin"],
-    weight: ["400", "500", "600"],
+    weight: ["300", "400", "500", "600", "700"],
     display: "swap",
     variable: "--font-work-sans",
 });
 
 export const rubik = Rubik({
     subsets: ["latin"],
-    weight: ["400", "500", "700"],
+    weight: ["400", "500", "600", "700"],
     display: "swap",
     variable: "--font-rubik",
 });
 
+export const lato = Lato({
+    subsets: ["latin"],
+    weight: ["300", "400", "700", "900"], // Choose the weights you need
+    display: "swap",
+    variable: "--font-lato", // Define the CSS variable
+});
+
 export const josefin = Josefin_Sans({
     subsets: ["latin"],
-    weight: ["300", "400", "500", "600", "700"],
+    weight: ["300", "400", "500", "600", "700"], // choose what you need
     display: "swap",
     variable: "--font-josefin",
 });
 
 export const playfair = Playfair_Display({
     subsets: ["latin"],
-    weight: ["400", "600", "700"],
+    weight: ["400", "500", "600", "700", "800", "900"],
     display: "swap",
     variable: "--font-playfair",
 });
